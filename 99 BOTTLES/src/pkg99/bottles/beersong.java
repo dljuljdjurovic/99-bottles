799d0df0ead7f3b9198d94a753a8a3622436fec7
Daniel Ljuljdjurovic <dljuljdjurovic@oakland.edu>
2016-09-30 14:30:09 -0700

    Finished the code for the 99 bottles method, verified it worsk

--- 99 BOTTLES/src/pkg99/bottles/beersong.java
+++ 99 BOTTLES/src/pkg99/bottles/beersong.java
@@ -14,6 +14,20 @@ public class beersong {
         ninety_nine_bottles_of_beer();
     }
     public static void ninety_nine_bottles_of_beer(){
-        
+             int numberbeer = 99;
+    String word = "bottles";
+    while (numberbeer > 0) {
+    System.out.println(numberbeer + " " + word + " of beer on the wall " + numberbeer + " " + word + " of beer.");
+      System.out.println("Take one down pass it around,");
+    numberbeer = numberbeer - 1;
+    if (numberbeer == 1) {
+        word = "bottle";
+      }
+    if (numberbeer > 0) {
+        System.out.println(numberbeer + " " + word + " of beer on the wall");
+      } else {
+        System.out.println("No more bottles of beer on the wall");
+      }
+    }
     }
 }
