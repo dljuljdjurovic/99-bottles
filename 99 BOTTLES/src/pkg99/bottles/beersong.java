d78cc020ed0c37bb032e40f01f4a850799a750bf
Daniel Ljuljdjurovic <dljuljdjurovic@oakland.edu>
2016-09-30 14:19:41 -0700

    created java file

---
+++ 99 BOTTLES/src/pkg99/bottles/beersong.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg99.bottles;
+
+/**
+ *
+ * @author Daniel
+ */
+public class beersong {
+    public static void main (String [] args){
+        
+    }
+    public static void ninety_nine_bottles_of_beer(){
+        
+    }
+}
